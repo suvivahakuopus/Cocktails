f0db6e60459313c8f86a6daf97779bdbc33783b4
suvivahakuopus <81032707+suvivahakuopus@users.noreply.github.com>
2021-10-19 21:44:35 +0300

    Delete root-reducer.js

--- root-reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux"; 
-import cocktailReducer from "./reducer";
-
-const rootReducer = combineReducers({
- data: cocktailReducer,
-
-});
-
-export default rootReducer;
